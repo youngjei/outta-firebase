095f3f7ab8e2eceb2896f056e61e5c192ff1acb5
SuYeonJn <suyeoni0218@gmail.com>
2022-05-06 12:42:18 +0900

    자료실 링크 연결

--- public/competition.js
+++ public/competition.js
@@ -1,20 +1,20 @@
 $(document).on('mouseenter', '.onetex' ,function(e) {
-    $('.one, .oneone, .oneonetwo, .onetwo').css('display', 'block');
-    $('.two, .twoone, .twotwo').css('display', 'none');
-    $('.thi, .thione').css('display', 'none');
-  })
-  
-  $(document).on('mouseenter', '.twotex' ,function(e) {
-    $('.one, .oneone, .oneonetwo, .onetwo').css('display', 'none');
-    $('.two, .twoone, .twotwo').css('display', 'block');
-    $('.thi, .thione').css('display', 'none');
-  })
-  
-  $(document).on('mouseenter', '.thitex' ,function(e) {
-    $('.one, .oneone, .oneonetwo, .onetwo').css('display', 'none');
-    $('.two, .twoone, .twotwo').css('display', 'none');
-    $('.thi, .thione').css('display', 'block');
-  })
+  $('.one, .oneone, .oneonetwo, .onetwo').css('display', 'block');
+  $('.two, .twoone, .twotwo, .twothree').css('display', 'none');
+  $('.thi, .thione').css('display', 'none');
+})
+
+$(document).on('mouseenter', '.twotex' ,function(e) {
+  $('.one, .oneone, .oneonetwo, .onetwo').css('display', 'none');
+  $('.two, .twoone, .twotwo, .twothree').css('display', 'block');
+  $('.thi, .thione').css('display', 'none');
+})
+
+$(document).on('mouseenter', '.thitex' ,function(e) {
+  $('.one, .oneone, .oneonetwo, .onetwo').css('display', 'none');
+  $('.two, .twoone, .twotwo, .twothree').css('display', 'none');
+  $('.thi, .thione').css('display', 'block');
+})
   
   function WeightChange(a){
     $(document).ready(function(){ 
@@ -25,9 +25,11 @@ $(document).on('mouseenter', '.onetex' ,function(e) {
         })}) 
     }
   
-  WeightChange('.oneone');
-  WeightChange('.oneonetwo');
-  WeightChange('.onetwo');
-  WeightChange('.twoone');
-  WeightChange('.twotwo');
-  WeightChange('.thione');
+
+    WeightChange('.oneone');
+    WeightChange('.oneonetwo');
+    WeightChange('.onetwo');
+    WeightChange('.twoone');
+    WeightChange('.twotwo');
+    WeightChange('.twothree');
+    WeightChange('.thione');
